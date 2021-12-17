f93270d6375e49cc3c17c29cb1b33c4fed9496bc
mitsumasaanan <mitsumasa.anan449@gmail.com>
2021-12-18 02:00:38 +0900

    コメント削除・修正機能実装

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use App\Article;
 use App\Comment;
 use Illuminate\Http\Request;
@@ -15,6 +16,23 @@ class CommentController extends Controller
         $comment->fill($request->all())->save();
         $article = Article::find($request->article_id);
         //return redirect()->route('article.show', compact('article'));
+        return redirect()->back();
+    }
+
+    public function edit(Comment $comment)
+    {
+        return view('comments.edit', ['comment' => $comment]);
+    }
+
+    public function update(CommentRequest $request, Comment $comment)
+    {
+        $comment->fill($request->all())->save();
+        return redirect()->route('articles.show', ['article' => $comment->article_id]);
+    }
+
+    public function destroy(Comment $comment)
+    {
+        $comment->delete();
         return redirect()->back();
     }
 }
