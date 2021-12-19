8adf93d53ba323834016db55950181628c1ba30e
mitsumasaanan <mitsumasa.anan449@gmail.com>
2021-12-20 08:34:33 +0900

    記事投稿部分のdivタグ閉じ位置修正(Vueエラー回避）

--- resources/views/articles/card.blade.php
+++ resources/views/articles/card.blade.php
@@ -11,26 +11,26 @@
             </div>
             <div class="font-weight-lighter">{{ $article->created_at->format('Y/m/d H:i') }}</div>
         </div>
-        @if( Auth::id() === $article->user_id )
-        <!-- dropdown -->
-        <div class="ml-auto card-text">
-            <div class="dropdown">
-                <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i class="fas fa-ellipsis-v"></i>
+    </div>
+    @if( Auth::id() === $article->user_id )
+    <!-- dropdown -->
+    <div class="ml-auto card-text">
+        <div class="dropdown">
+            <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-ellipsis-v"></i>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right">
+                <a class="dropdown-item" href="{{ route("articles.edit", ['article' => $article]) }}">
+                    <i class="fas fa-pen mr-1"></i>記事を更新する
                 </a>
-                <div class="dropdown-menu dropdown-menu-right">
-                    <a class="dropdown-item" href="{{ route("articles.edit", ['article' => $article]) }}">
-                        <i class="fas fa-pen mr-1"></i>記事を更新する
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item text-danger" data-toggle="modal" data-target="#modal-delete-{{ $article->id }}">
-                        <i class="fas fa-trash-alt mr-1"></i>記事を削除する
-                    </a>
-                </div>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item text-danger" data-toggle="modal" data-target="#modal-delete-{{ $article->id }}">
+                    <i class="fas fa-trash-alt mr-1"></i>記事を削除する
+                </a>
             </div>
         </div>
-        <!-- dropdown -->
     </div>
+    <!-- dropdown -->
 
     <!-- modal -->
     <div id="modal-delete-{{ $article->id }}" class="modal fade" tabindex="-1" role="dialog">
