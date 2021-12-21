4a68f5b72c7537aec2f013a30150b5b3501e189a
mitsumasaanan <mitsumasa.anan449@gmail.com>
2021-12-21 17:29:45 +0900

    いいねされているか判定するメソッドのテスト

--- tests/Feature/ArticleTest.php
+++ tests/Feature/ArticleTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Article;
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -11,6 +12,7 @@ class ArticleTest extends TestCase
 {
     use RefreshDatabase;
 
+    //いいねされているかを判定するメソッドのテスト
     public function testIsLikedByNull()
     {
         $article = factory(Article::class)->create();
@@ -20,6 +22,32 @@ class ArticleTest extends TestCase
         $this->assertFalse($result);
     }
 
+    //いいねされているかを判定するメソッド
+    public function testIsLikedByTheUser()
+    {
+        $article = factory(Article::class)->create();
+        $user = factory(User::class)->create();
+        $article->likes()->attach($user);
+
+        $result = $article->isLikedBy($user);
+
+        $this->assertTrue($result);
+    }
+
+    //いいねをしていないケースのテスト
+    public function testIsLikedByAnother()
+    {
+        $article = factory(Article::class)->create();
+        $user = factory(User::class)->create();
+        $another = factory(User::class)->create();
+        $article->likes()->attach($another);
+
+        $result = $article->isLikedBy($user);
+
+        $this->assertFalse($result);
+    }
+
+    //ログイン前ユーザーが記事更新ページに行くとログイン画面にリダイレクトされるかテスト
     public function testGuestUpdate()
     {
         $article = factory(Article::class)->create();
