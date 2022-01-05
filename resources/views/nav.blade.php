378f412d79101537ed04d66471df19e2572dca8c
mitsumasaanan <mitsumasa.anan449@gmail.com>
2022-01-05 18:42:56 +0900

    ヘッダーメニュー修正

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -35,19 +35,13 @@
 
         @auth
         <!-- Dropdown -->
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-user-circle"></i>
-            </a>
-            <div class="dropdown-menu dropdown-menu-right dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
-                <button class="dropdown-item" type="button" onclick="location.href='{{ route("users.show", ["name" => Auth::user()->name]) }}'">
-                    マイページ
-                </button>
-                <div class="dropdown-divider"></div>
-                <button form="logout-button" class="dropdown-item" type="submit">
-                    ログアウト
-                </button>
-            </div>
+        <li class="nav-item menu__item">
+            <a class="nav-link" href="{{ route("users.show", ["name" => Auth::user()->name]) }}">マイページ</a>
+        </li>
+        <li class="nav-item menu__item">
+            <button form="logout-button" class="nav-link logout-button" type="submit">
+                ログアウト
+            </button>
         </li>
         <form id="logout-button" method="POST" action="{{ route('logout') }}">
             @csrf
